b1ee1f12cf720bb2ebb888f151262b6f65087fe3
Fitsum Mehari <m.fitsummehari@gmail.com>
2022-07-29 22:14:09 +0300

    routes for user login and register work also for uploading and getting file work

---
+++ routes/verifyToken.js
@@ -0,0 +1,58 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+    if (!authHeader) {
+        return res.status(401).json({
+            "error": {
+                "errors": [
+                    {
+                        "domain": "global",
+                        "reason": "required",
+                        "message": "Login Required",
+                        "locationType": "header",
+                        "location": "Authorization"
+                    }
+                ],
+                "code": 401,
+                "message": "Login Required"
+            }
+        });
+    } else {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, "token key", (error, user) => {
+            if (error) {
+                res.status(403).json("Token is incorrect!")
+            } else {
+                req.user = user;
+                next();
+            }
+        })
+    }
+}
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.userId || req.user.isAdmin) {
+            next();
+        } else {
+            res.status(401).json({
+                "error": {
+                    "errors": [
+                        {
+                            "domain": "global",
+                            "reason": "required",
+                            "message": "Login Required",
+                            "locationType": "header",
+                            "location": "Authorization"
+                        }
+                    ],
+                    "code": 401,
+                    "message": "Login Required"
+                }
+            });
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
